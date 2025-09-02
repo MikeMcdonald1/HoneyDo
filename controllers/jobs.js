5662a9fc0156e6262e9d955c948b6ef194fa50a0
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-09-02 17:41:28 -0400

    feat(jobs): add delete job controller logic and create Postman request for DELETE method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -52,7 +52,20 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send("Delete Job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
