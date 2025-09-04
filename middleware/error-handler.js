6f25db6d2d2ba9fd872ec1fb7ca00b73b1913714
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-09-04 16:53:56 -0400

    feat: return 400 status code with user-friendly message for validation errors (missing/invalid fields)

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,3 @@
-const { login } = require("../controllers/auth");
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -9,8 +7,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = 400;
   }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
