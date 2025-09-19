fda98e5186255f00197918c95fcda0aa3249a7a3
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-09-19 18:12:13 -0400

    feat: add public folder and index.html file with updated data for task model

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.use(express.static("public"));
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authenticateUser, taskRouter);
